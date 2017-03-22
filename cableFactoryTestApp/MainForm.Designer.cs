304acf26d9c108eda92fbf2dfb267f4aaf2b7fd8
cwatzkb <blake.watzke@jci.com>
2017-03-22 10:25:07 -0500

    changed comm settings

--- cableFactoryTestApp/MainForm.Designer.cs
+++ cableFactoryTestApp/MainForm.Designer.cs
@@ -301,6 +301,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "Cable Twist/Pull Test App";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
