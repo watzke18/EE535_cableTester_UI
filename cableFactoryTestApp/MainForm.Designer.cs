cb41aa2d5f13d1bb98a854876179d2fb93faf210
Blake <bmwatzke@uwm.edu>
2017-04-13 19:52:59 -0500

    changed size of mainform

--- cableFactoryTestApp/MainForm.Designer.cs
+++ cableFactoryTestApp/MainForm.Designer.cs
@@ -164,7 +164,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(905, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(902, 28);
             this.menuStrip1.TabIndex = 15;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -382,7 +382,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 407);
+            this.ClientSize = new System.Drawing.Size(902, 407);
             this.Controls.Add(this.comboBoxRefreshRate);
             this.Controls.Add(this.labelRefreshRate);
             this.Controls.Add(this.labelBoxAmbientTemp);
